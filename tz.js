6c2213ee7486921714e5bbdbfc9b3eb7dca02146
Tarun Balaji Ravi <tarunbalaji3@gmail.com>
2022-03-03 14:51:54 -0500

    Update Command Line Utility

--- tz.js
+++ tz.js
@@ -1,17 +1,38 @@
+const { time } = require('console');
 const moment = require('moment-timezone');
+
+const yargs = require('yargs');
 
 moment.tz.setDefault('America/New_York');
 
+// console.log(yargs.argv);
+
 let targetTimezone;
 
-if (process.argv.length !== 3) {
+// console.log(targetTimezone);
+
+if (yargs.argv._.length === 0) {
     console.log("Usage: node <script-file> <timezone>");
     process.exit(1);
 }
 else {
-    targetTimezone = process.argv[2];
+    targetTimezone = yargs.argv._[0];
 }
 
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+const flag = yargs.argv._[0];
+// console.log(targetTimezone);
+// console.log(yargs.argv);
+console.log(flag);
+
+
+/* if (yargs.argv._[1] === "true") {
+    console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+}
+else {
+    console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+} */
+
+
+//console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
 
 
